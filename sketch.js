0866eb12445bf32f6b0e9357729d5c5b14edfdad
sumaiyahx <gomailgo.sm@gmail.com>
2025-10-13 11:45:06 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let house = 33;
+var mouse = 66;
+let name = "sumaiyah";
+let hot = true;
+let temp = 55.88;
+let idontknow;
+
+funtion setup () {
+  let myWidth - 640;
+  createCanvas (myWidth, myWidth);
+  let person = 55;
+}
+
+function draw () {
+  background (220);
+  //circle (50, 50, 50);
+  circle (house, 50, 25);
+  console.log(house)
+  house = house + 1;
+}
+
+
+
+function setup() {
+
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(200, 200, 50);
+
+}
